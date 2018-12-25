85ace5f61e833dd235a1f904e3483ee8e23cfa71
sunTimer <757398890@qq.com>
2018-12-25 10:05:07 +0800

    Merge branch 'master' of https://github.com/sunTimer/aladdin
    # Conflicts:
    #	src/main/java/Test.java

--- aladdin-j8/src/main/java/lambda/model/Album.java
+++ aladdin-j8/src/main/java/lambda/model/Album.java
@@ -1,6 +1,12 @@
 package lambda.model;
 
-public class Album {
+import lombok.Data;
 
-    
+import java.util.List;
+
+@Data
+public class Album {
+    private String name;
+    private List<Track> tracks;
+    private List<Artist> musicians;
 }
