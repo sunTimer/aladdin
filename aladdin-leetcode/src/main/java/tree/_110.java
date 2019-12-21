8a95c30e72b69a99115ffde9eeaebc391d366026
sunTimer <757398890@qq.com>
2019-12-21 18:25:14 +0800

    tree

---
+++ aladdin-leetcode/src/main/java/tree/_110.java
@@ -0,0 +1,40 @@
+package tree;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class _110 {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (isB(root)) {
+            return isBalanced(root.left) && isBalanced(root.right);
+        }
+        return false;
+    }
+
+    public boolean isB(TreeNode root) {
+        if (Math.abs(dep(root.left, 0) - dep(root.right, 0)) > 1) {
+            return false;
+        }
+        return true;
+    }
+
+    public int dep(TreeNode root, int depth) {
+        if (root == null) {
+            return depth;
+        }
+        depth++;
+        return Math.max(dep(root.left, depth), dep(root.right, depth));
+    }
+
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode(0);
+        root.left = new TreeNode(1);
+        root.left.right = new TreeNode(2);
+        root.right = new TreeNode(0);
+        Assert.assertTrue(isBalanced(root));
+    }
+}
