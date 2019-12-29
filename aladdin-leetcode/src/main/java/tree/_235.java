cbb2ba6a6623b769acb0d6d97be6fde7d294d758
sunTimer <757398890@qq.com>
2019-12-29 16:21:55 +0800

    tree

---
+++ aladdin-leetcode/src/main/java/tree/_235.java
@@ -0,0 +1,14 @@
+package tree;
+
+public class _235 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+}
