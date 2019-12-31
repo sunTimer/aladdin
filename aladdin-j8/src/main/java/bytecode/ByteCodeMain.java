bd98726a1346e4bd34711dd258c019372f1d6680
sunTimer <757398890@qq.com>
2019-12-31 13:18:35 +0800

    linkedlist

--- aladdin-j8/src/main/java/bytecode/ByteCodeMain.java
+++ aladdin-j8/src/main/java/bytecode/ByteCodeMain.java
@@ -1,13 +1,86 @@
 package bytecode;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ByteCodeMain {
 
+    static Lock lock = new ReentrantLock();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+
+
+        MyClassLoader myClassLoader = new MyClassLoader();
+        Class<?> aClass = myClassLoader.loadClass("bytecoded.User");
+        User o = (User) aClass.newInstance();
+        System.out.println(o.getClass().getClassLoader());
+        System.out.println(o);
+        User user = new User();
+        System.out.println(user.getClass().getClassLoader());
+
         try {
-            User user = new User();
+            lock.lock();
+            user.setCode('d');
+            user.size();
+            lock.unlock();
+            System.out.println(user.toString());
+            int a = SubClass.a;
+            SubClass.setA(3);
+            if (a > 5) {
+                SubClass.a = 5;
+            } else {
+                SubClass.a = 10;
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+}
+
+class StaticClass {
+    static int a;
+
+    static {
+        a = 3;
+    }
+
+    public static void setA(int b) {
+        a = b;
+    }
+}
+
+class SubClass extends StaticClass {
+
+}
+
+class MyClassLoader extends ClassLoader {
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        Class clazz = null;
+        String classFilename = name + ".class";
+        File classFile = new File(classFilename);
+        if (classFile.exists()) {
+            try (FileChannel fileChannel = new FileInputStream(classFile)
+                    .getChannel()) {
+                MappedByteBuffer mappedByteBuffer = fileChannel
+                        .map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+                byte[] b = mappedByteBuffer.array();
+                clazz = defineClass(name, b, 0, b.length);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (clazz == null) {
+            throw new ClassNotFoundException(name);
+        }
+        return clazz;
     }
 }
---
+++ aladdin-leetcode/src/main/java/linkedlist/ListNode.java
@@ -0,0 +1,11 @@
+package linkedlist;
+
+public class ListNode {
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public int val;
+    public ListNode next;
+}
