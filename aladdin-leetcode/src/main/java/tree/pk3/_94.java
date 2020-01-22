a23ffb7bc6b324a19e58218847ef8b3b4e387a76
sunTimer <757398890@qq.com>
2020-01-22 22:39:35 +0800

    tree travel

---
+++ aladdin-leetcode/src/main/java/tree/pk3/_94.java
@@ -0,0 +1,61 @@
+package tree.pk3;
+
+import org.junit.Test;
+import tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给定一个二叉树，返回它的中序 遍历。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [1,null,2,3]
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * <p>
+ * 输出: [1,3,2]
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class _94 {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> ret = new ArrayList<>();
+
+        if (root == null) {
+            return ret;
+        }
+
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            ret.add(curr.val);
+            curr = curr.right;
+        }
+        return ret;
+    }
+
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        System.out.println(inorderTraversal(root));
+    }
+}
